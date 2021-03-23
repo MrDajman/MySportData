18ccf119a71eb2d799010db986eb038d5ce5cee8
MrDajman <mr.dajman@yahoo.pl>
2021-03-23 22:27:00 +0100

    added flask server and ploting the activities on openstreetmaps

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import polyline
 import cv2
 from operator import itemgetter
 import numpy as np
+from flask import Flask
+import folium
+
+app = Flask(__name__)
 
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
@@ -98,55 +102,6 @@ def get_my_activities(access_token,before = False, after = False, page = False, 
     #print(len(r.json()))
     return r.json()
 
-def print_activity_map(activity):
-    activity = activity
-    activity_map = activity["map"]
-    activity_points = np.array(polyline.decode(activity_map["polyline"]))
-    activity_points = activity_points * 10**6
-    activity_points = np.array(activity_points, dtype = int)
-    #activity_points[:,1] = activity_points[:,1] * -1
-    #activity_points[:,0] = activity_points[:,0] * -1
-    #print (activity_points)
-    activity_points[:,[0, 1]] = activity_points[:,[1, 0]]
-    activity_points[:,1] = activity_points[:,1] * -1
-    # Y, X notation
-    max_Y_point = (max(activity_points,key=itemgetter(0)))
-    max_X_point = (max(activity_points,key=itemgetter(1)))
-    min_Y_point = (min(activity_points,key=itemgetter(0)))
-    min_X_point = (min(activity_points,key=itemgetter(1)))
-
-    #print(max_X_point, max_Y_point, min_X_point, min_Y_point)
-
-    img_height = int((max_X_point[1] - min_X_point[1]))
-    img_width = int((max_Y_point[0] - min_Y_point[0]))
-
-    subtract_matrix_Y = np.zeros((activity_points.shape),dtype= int)
-    subtract_matrix_Y[:,0] = 1
-
-    subtract_matrix_X = np.zeros((activity_points.shape),dtype= int)
-    subtract_matrix_X[:,1] = 1
-
-    activity_points = activity_points - subtract_matrix_Y * min_Y_point[0] - subtract_matrix_X * min_X_point[1]
-
-    #print(activity_points)
-
-    map_image = np.zeros((img_height,img_width,3), np.uint8)
-
-    #activity_points 
-
-    #activity_points = [tuple(pair) for pair in activity_points]
-    #print(activity_points)
-
-    cv2.drawContours(map_image, [activity_points], 0, (120,120,120),5 )
-    cv2.imwrite("img.jpg", map_image) 
-    #cv2.imshow("map", map_image)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    #print(img_height, img_width)
-
-
-
 def check_response(response):
     if response.ok:
         print("INFO:\tSuccessfully retrieved request")
@@ -157,18 +112,76 @@ def check_response(response):
             print("ERROR:\t{}: {}".format(error["resource"],error["code"]))
         exit()
 
+class Activity:
+    #constructor
+    def __init__(self, id):
+        print("INFO:\tRetrieving activity ID: {}".format(id))
+        url = "https://www.strava.com/api/v3/activities/{}?".format(id)
+        r = requests.get(url, data = {"access_token":access_token})
+        check_response(r)
 
-def get_activity_by_id(id):
-    print("INFO:\tRetrieving activity ID: {}".format(id))
-    url = "https://www.strava.com/api/v3/activities/{}?".format(id)
-    r = requests.get(url, data = {"access_token":access_token})
-    check_response(r)
-    #with open('my_activities.json', 'w') as outfile:
-    #    json.dump(r.json(), outfile, sort_keys=True,)
+        self.data = r.json()
+        self.polyline = r.json()["map"]["polyline"]
+
+    def get_polyline(self):
+        return self.polyline
+
+    def print_activity_map_cv(self, show = False):
+        print("INFO:\tRetrieving activity ID: {}".format(id))
+        activity_points = np.array(polyline.decode(self.polyline))
+        activity_points = activity_points * 10**5
+        activity_points = np.array(activity_points, dtype = int)
+        
+        #activity_points[:,1] = activity_points[:,1] * -1
+        #activity_points[:,0] = activity_points[:,0] * -1
+        #print (activity_points)
+        activity_points[:,[0, 1]] = activity_points[:,[1, 0]]
+        activity_points[:,1] = activity_points[:,1] * -1
+        # X,Y notation
+        max_Y_point = (max(activity_points,key=itemgetter(0)))
+        max_X_point = (max(activity_points,key=itemgetter(1)))
+        min_Y_point = (min(activity_points,key=itemgetter(0)))
+        min_X_point = (min(activity_points,key=itemgetter(1)))
+
+        #print(max_X_point, max_Y_point, min_X_point, min_Y_point)
+
+        img_height = int((max_X_point[1] - min_X_point[1]))
+        img_width = int((max_Y_point[0] - min_Y_point[0]))
+
+        subtract_matrix_Y = np.zeros((activity_points.shape),dtype= int)
+        subtract_matrix_Y[:,0] = 1
+
+        subtract_matrix_X = np.zeros((activity_points.shape),dtype= int)
+        subtract_matrix_X[:,1] = 1
+
+        activity_points = activity_points - subtract_matrix_Y * min_Y_point[0] - subtract_matrix_X * min_X_point[1]
+
+        print(img_height, img_width)
+
+        map_image = np.zeros((img_height,img_width,3), np.uint8)
+
+        cv2.drawContours(map_image, [activity_points], 0, (120,120,120),15)
+
+        #print(map_image)
+
+        cv2.imwrite("img.jpg", map_image) 
+        if show:
+            cv2.imshow("map", map_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+@app.route('/')
+def index():
+    start_coords = (48.040700, 2.7360300)
+    #line = [(46.9540700, 142.7360300), (46.5940700, 142.3760300)]
+    folium_map = folium.Map(location=start_coords, zoom_start=10)
+    for activity in activities:
+        a = Activity(activity["id"])
+        line = polyline.decode(a.get_polyline())
+        folium.PolyLine(line).add_to(folium_map)
+
+    return folium_map._repr_html_()
     
-    #print(len(r.json()))
-    return r.json()
-
 if __name__ == "__main__":
     if startupCheck("strava_tokens.json"):
         with open('strava_tokens.json') as json_file:
@@ -180,85 +193,18 @@ if __name__ == "__main__":
             print("INFO:\tTokens are up to date. Next refresh in {} minutes".format(round((strava_tokens['expires_at'] - time.time())/60.0,2)))
         print("INFO:\tAccess token: {}".format(access_token))
 
-        activities = get_my_activities(access_token,per_page=10)
-        #print(activities[0]["id"])
-        one_activity = get_activity_by_id(activities[0]["id"])
-        #print(one_activity)
-        print_activity_map(one_activity)
+        activities = get_my_activities(access_token,per_page=30)
+
+        #a1 = Activity(activities[0]["id"])
+        #a1.print_activity_map_cv()
+
+        app.run(debug=True)
+
+        #one_activity = get_activity_by_id(activities[0]["id"])
+        #print_activity_map(one_activity)
 
     else:
         # retrieve code from the link
         # https://www.strava.com/oauth/authorize?client_id=63388&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=profile:read_all,activity:read_all
         code = '81152c10599d233cf7d92f050991dafbdbdb010b'
         first_auth(code)
-
-    
-
-
-
-
-exit()
-
-
-
-
-'''
-Run this fuction to receive access token
-https://www.strava.com/oauth/authorize?client_id=63388&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=profile:read_all,activity:read_all
-
-def request_token(access_token):
-    auth_url = "https://www.strava.com/oauth/token"
-
-    payload = {
-        'client_id': "63388",
-        'client_secret': 'cd53d9a8623c88f85fe7f59ca0c4e9a4e6c2ac5f',
-        'code': '9c13046e3a9cfaaf30f9580b79f239f26bdf1038',
-        'grant_type': "authorization_code"
-    }
-
-
-                        url = 'https://www.strava.com/oauth/token',
-                        data = {
-                                'client_id': [INSERT_CLIENT_ID_HERE],
-                                'client_secret': '[INSERT_CLIENT_SECRET_KEY]',
-                                'grant_type': 'refresh_token',
-                                'refresh_token': strava_tokens['refresh_token']
-                                }
-
-    print("Requesting Token...\n")
-    res = requests.post(auth_url, data=payload, verify=False)
-    print(res)
-    print(res.json())
-    print(res.ok)
-    if res.ok:
-        print("Successfully requested ")
-    else:
-        return 0
-    print(res.json())
-    access_token = res.json()['access_token']
-    print("Access Token = {}\n".format(access_token))
-    return res.json()
-
-access_token = 0 
-access_token_res = request_token(access_token)
-access_token= access_token_res["access_token"]
-header = {'Authorization': 'Bearer ' + access_token}
-param = {'per_page': 200, 'page': 1}
-#my_dataset = requests.get(activites_url, headers=header, params=param).json()
-#print(my_dataset[0]["name"])
-#print(my_dataset[0]["map"]["summary_polyline"])
-
-
-end_url = "https://www.strava.com/api/v3/activities/4949939963"
-my_data = requests.get(end_url, headers=header, params=param).json()
-print(my_data)
-
-
-
-#code=792f817b9d623ba0319c9b72fa310661a3a4b967
-#cod e d48feacb8f1862aa5fa0633ee87d0a08e8d82c47
-#{"token_type": "Bearer", "expires_at": 1616295018, "expires_in": 12697,
-# "refresh_token": "9ff7242e6a89b7d152c1ce613bcc94a7814f590a",
-# "access_token": "8aec62deadc7e07cc47ecd40cd76f9d8a2dd43b7", 
-
-# token b3ae133fadc1794e19812cc94f2a62a4fc4c58e7'''
